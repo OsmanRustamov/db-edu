cefb8c45907160bec416d3e0355041706756a394
Osman <osmanrustamovbk@gmail.com>
2023-02-15 10:19:57 +0500

    create hosting_db

--- postgres/migration/20230202_00_create_table_classes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS classes (
-    id SERIAL NOT NULL,
-    name TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (name)
-);
-
-COMMENT ON COLUMN classes.id IS 'Идентификатор класса обслуживания';
-COMMENT ON COLUMN classes.name IS 'Название класса обслуживания';
